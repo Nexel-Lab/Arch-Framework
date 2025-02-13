9959a75e8e433774a3f5ca7e2239bcb92c5b4e7e
Jirayu-ninl <Jirayu.ninl@gmail.com>
2025-02-13 18:54:35 +0700

    [misc] deps: remove subModule -> install separately instead

--- global/config/app.ts
+++ global/config/app.ts
@@ -2,13 +2,13 @@
 
 export const app = {
   NAME: 'Arch Framework',
-  VERSION: '2024.12.0101',
-  UPDATE_DATE: 'Dec 1, 2024',
+  VERSION: '2025.02.1301',
+  UPDATE_DATE: 'Feb 13, 2025',
   SENTRY: {
     dsn: process.env.NEXT_PUBLIC_SENTRY_DSN ?? '',
   },
   Dependencies: {
-    Arch: '1.0',
+    Arch: '1.1.0',
     React: '18.3.1',
     NextJs: '14.2.18',
   },
